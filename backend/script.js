d758ce051874303f31df86451f0e22baa728cf0e
Elizeu Moreira <126178625+ElizeuWinters@users.noreply.github.com>
2024-02-23 12:10:31 -0300

    Add files via upload

---
+++ backend/script.js
@@ -0,0 +1,28 @@
+let nome;
+nome = prompt("Digite seu nome:");
+console.log(nome);
+window.alert("olá $ {nome}!");
+let numero = number.parseint(math.random()*10 + 1);
+let tentativas = 0;
+let palpite = 0;
+
+
+while((palpite != numero)&&(tentativas <5)){
+    palpite = prompt("digite um numero:");
+    tentativas++;
+    if(palpite > numero){
+        window.alert("você errou! o numero é menor que ${palpite}. tente outra vez");
+    }else{
+        if(palpite < numero)
+        window.alert("você errou! o numero é maior que ${palpite}. tente outra vez");
+}
+}
+
+if(numero == palpite){
+    let palavratentativa = tentativas==1 ? "tentativa" : "tentativas"
+
+    window.alert("parabens ${nome}! o numero era ${numero}, e você acerou com $(tentativas) $ (palavratentativa)!!");
+
+}else{
+    window.alert("você esgotou suas tentativas e não acerotou, o numero era${numero}! -")
+}
